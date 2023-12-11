6d7c1f15cc62ae94d3dffe63719685cce1cb8b27
Manzen <51030517+manzenn@users.noreply.github.com>
2023-12-11 09:38:43 +0300

    def_title.py

--- def_title.py
+++ def_title.py
@@ -1,4 +1,5 @@
-def title(string):
+def title(input: str) -> str:
+    string = input
     flag = True
     up_word = []
     for i in range (len(string)):
@@ -12,5 +13,12 @@ def title(string):
             up_word.append(string[i])
     return ''.join(up_word)
 
+def title1(input: str) -> str: 
+    string = input.split()
+    title_words = [word[0].upper() + word[1:] for word in string] 
+    return ' '.join(title_words)
+
 test = "тесТОвое задание    для pt"
+
 print(title(test))
+print(title1(test))
